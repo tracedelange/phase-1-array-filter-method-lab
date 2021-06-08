36a1f2178a5ed2d9688441ff1702c7fac0861124
Trace <tracedelange@me.com>
2021-06-08 13:10:48 -0500

    commit tree 315ee0106c40e5bf359ba2266d07fcabf87e180e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, query){
+    return drivers.filter(function(driver){
+        return (driver.toLowerCase() == query.toLowerCase())
+    })
+}
+
+function fuzzyMatch(drivers, query){
+    return (drivers.filter(function(driver){
+        return (driver.slice(0,2) == query)
+    }))
+}
+
+function matchName(drivers, query){
+    return (drivers.filter(function(driver){
+        return (driver.name == query)
+    }))
+}
